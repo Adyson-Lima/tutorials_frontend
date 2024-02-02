d4a817c3f4700a150a0ab09b3bfbc12984a4f156
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-02-02 09:19:08 -0300

    25° commit - logica update implementada

--- src/pages/Tutorials/index.js
+++ src/pages/Tutorials/index.js
@@ -13,6 +13,15 @@ export default function Tutorials(){
     .then(response => {setTutorials(response.data)})
   }, []);
 
+  // update, navega para NewUpdate
+  async function updateTutorial(id){
+    try {
+      navigate(`/newupdate/${id}`);
+    } catch (error) {
+      alert('Erro ao acessar NewUpdate');
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -42,7 +51,8 @@ export default function Tutorials(){
                 <td>
 
                   <button data-testid="mybtn1" type="button"
-                  className="btn btn-outline-info" style={{margin: '2px'}}>Editar</button>
+                  className="btn btn-outline-info" style={{margin: '2px'}}
+                  onClick={() => updateTutorial(tutorial.id)}>Editar</button>
 
                   <button data-testid="mybtn2" type="button"
                   className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
